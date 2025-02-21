fd21bbe1136a2c750f0f981c9293f4ec1661047a
Niladri Sekhar Jyoti <niladri123@gmail.com>
2025-02-21 14:30:01 -0500

    Added Controller Structure

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,47 +1,18 @@
 const express = require('express');
 const campsiteRouter = express.Router();
+const campsiteController = require('../controllers/campsiteController');
 
-// Handling the '/campsites' route
+// Routes using the Controller methods
 campsiteRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end('Will send all the campsites to you');
-})
-.post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
-.put((req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-})
-.delete((req, res) => {
-    res.end('Deleting all campsites');
-});
+    .get(campsiteController.getAllCampsites)
+    .post(campsiteController.addCampsite)
+    .put((req, res) => res.status(403).end('PUT operation not supported on /campsites'))
+    .delete(campsiteController.deleteAllCampsites);
 
-// Handling the '/campsites/:campsiteId' route
 campsiteRouter.route('/:campsiteId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-})
-.post((req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-})
-.put((req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
-.delete((req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+    .get(campsiteController.getCampsiteById)
+    .post((req, res) => res.status(403).end(`POST operation not supported on /campsites/${req.params.campsiteId}`))
+    .put(campsiteController.updateCampsite)
+    .delete(campsiteController.deleteCampsite);
 
 module.exports = campsiteRouter;
