d1c042bd5ab55cd3e7156f457679c01a555e2f5a
Niladri Sekhar Jyoti <niladri123@gmail.com>
2025-02-21 19:04:05 -0500

    Used Nodemon to see auto updates

--- myapp/routes/team.js
+++ myapp/routes/team.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
     res.render('team', { title: 'Our Team' });
 });
 
+/* GET info about the team members. */
+router.get('/members', function(req, res, next) {
+    res.render('team', { title: 'Team Members' });
+});
+
 module.exports = router;
